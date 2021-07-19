776a63fe7a3c445bbb68bd844d74f18dfda45a3f
nurfawaiq <nurfawaiq@gmail.com>
2021-07-19 13:04:27 +0700

    authentication implemented

--- src/api/notes/routes.js
+++ src/api/notes/routes.js
@@ -3,26 +3,41 @@ const routes = (handler) => [
         method: 'POST',
         path: '/notes',
         handler: handler.postNoteHandler, // postNoteHandler hanya menerima dan menyimpan "satu" note.
+        options: {
+            auth: 'notesapp_jwt',
+        },
     },
     {
         method: 'GET',
         path: '/notes',
         handler: handler.getNotesHandler, // getNotesHandler mengembalikan "banyak" note.
+        options: {
+            auth: 'notesapp_jwt',
+        },
     },
     {
         method: 'GET',
         path: '/notes/{id}',
         handler: handler.getNoteByIdHandler, // getNoteByIdHandler mengembalikan "satu" note.
+        options: {
+            auth: 'notesapp_jwt',
+        },
     },
     {
         method: 'PUT',
         path: '/notes/{id}',
         handler: handler.putNoteByIdHandler, // putNoteByIdHandler hanya menerima dan mengubah "satu" note.
+        options: {
+            auth: 'notesapp_jwt',
+        },
     },
     {
         method: 'DELETE',
         path: '/notes/{id}',
         handler: handler.deleteNoteByIdHandler,
+        options: {
+            auth: 'notesapp_jwt',
+        },
     },
 ];
 
